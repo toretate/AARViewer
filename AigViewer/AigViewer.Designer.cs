9d6cd1f88c8a75c60d7fc468acf7254b569b6503
toretatenee <toretatenee@gmail.com>
2024-04-19 23:52:42 +0900

    AIGファイルを読み込んで画像化するところだった・・・ような・・・気が…

---
+++ AigViewer/AigViewer.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace AigViewer
+{
+    partial class AigViewer
+    {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 150);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.SizeChanged += new System.EventHandler(this.pictureBox1_SizeChanged);
+            // 
+            // AigViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "AigViewer";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ AigViewer/AigViewer.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+// AIGファイルの処理
+
+namespace AigViewer
+{
+    public partial class AigViewer : UserControl
+    {
+        private enum ColorType { RGBA, BGRA, RGBQuad, UNKNOWN, }
+
+        private ColorType m_colorType;
+        private Bitmap m_canvas;
+
+        public AigViewer()
+        {
+            InitializeComponent();
+        }
+
+        // AIGStreamかどうかを調べる
+        public static bool IsAig( Stream aigStream ) {
+            bool result;
+            long position = aigStream.Position;
+            aigStream.Position = 0;
+
+            BinaryReader reader = new BinaryReader(aigStream);
+            {
+                // ALIG で始まるかどうか調査
+                byte[] magic = reader.ReadBytes(4);
+                byte[] alig = new byte[] { (byte)'A', (byte)'L', (byte)'I', (byte)'G' };
+                result = alig.SequenceEqual(magic);
+            }
+            aigStream.Position = position;
+
+            return result;
+        }
+
+        public void LoadPicture(BinaryReader reader)
+        {
+            if (IsAig(reader.BaseStream) == false) return;
+            reader.BaseStream.Position = 0;
+            reader.ReadBytes(4);    // "ALIG"
+
+            {
+                // 4byte読み飛ばす(何のフラグか不明)
+                byte[] flags1 = reader.ReadBytes(4);
+
+                // RGBA/BGRA を判定
+                {
+                    byte[] type = reader.ReadBytes(4);
+                    byte[] rgba = new byte[] { (byte)'R', (byte)'G', (byte)'B', (byte)'A' };
+                    byte[] bgra = new byte[] { (byte)'B', (byte)'G', (byte)'R', (byte)'A' };
+                    byte[] abg4 = new byte[] { (byte)'A', (byte)'B', (byte)'G', (byte)'4' };
+                    if (type.SequenceEqual(rgba))
+                    {
+                        m_colorType = ColorType.RGBA;
+                    }
+                    else if (type.SequenceEqual(bgra))
+                    {
+                        m_colorType = ColorType.BGRA;
+                    }
+                    else if (type.SequenceEqual(abg4))
+                    {
+                        m_colorType = ColorType.RGBQuad;
+                    }
+                    else
+                    {
+                        m_colorType = ColorType.UNKNOWN;
+                        return;
+                    }
+                }
+
+                // 4byte読み飛ばす(何のフラグか不明)
+                byte[] flags2 = reader.ReadBytes(4);
+
+                // 謎の16byte読み飛ばす
+                byte[] flags3 = reader.ReadBytes(16);
+
+                // 画像をロード
+                {
+                    int width = 1024;
+                    byte[] data = new byte[4];
+                    Stream stream = reader.BaseStream;
+                    int height = (int)(stream.Length / (width * data.Length)) + 100;
+                    m_canvas = new Bitmap(width, height);
+                    Graphics g = Graphics.FromImage(m_canvas);
+
+                    long i = 0, y = 0;
+                    while (i < stream.Length)
+                    {
+                        for (long x = 0; x < width; x++)
+                        {
+                            data = reader.ReadBytes(4);
+                            if (data.Length < 4) break;
+
+                            if (m_colorType == ColorType.RGBA)
+                            {
+                                Color c = Color.FromArgb(data[3], data[0], data[1], data[2]);
+                                m_canvas.SetPixel((int)x, (int)y, c);
+                            }
+                            else if( m_colorType == ColorType.BGRA)
+                            {
+                                Color c = Color.FromArgb(data[3], data[2], data[1], data[0]);
+                                m_canvas.SetPixel((int)x, (int)y, c);
+                            }
+                            else if (m_colorType == ColorType.RGBQuad)
+                            {
+                                Color c = Color.FromArgb(255, data[2], data[1], data[0]);
+                                m_canvas.SetPixel((int)x, (int)y, c);
+                            }
+                        }
+                        if (data.Length < 4) break;
+                        i += width * data.Length;
+                        y++;
+                    }
+
+                    g.Dispose();
+                    pictureBox1.Location = new Point(0, 0);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    pictureBox1.Image = m_canvas;
+
+                }
+            }
+        }
+
+        private void pictureBox1_SizeChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Size = new Size(new Point(m_canvas.Width, m_canvas.Height));
+        }
+    }
+}
